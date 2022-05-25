e0fc9a2137a9ce2aabb1dfe0e4e7260a2a7db005
侯智洋 <houzhiyang1997@163.com>
2022-05-25 22:36:18 +0800

    实现点击英雄头像跳转详情页，打通数据流

--- src/views/Team/ChessInfo.vue
+++ src/views/Team/ChessInfo.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>chessinfo---{{ id }}</div>
+  <div class="chessinfo-container">
+    <detail-header title="英雄详情"></detail-header>
+    <div>chessinfo---{{ id }}</div>
+    <div>chessinfo---{{ id }}</div>
+    <div>chessinfo---{{ id }}</div>
+    <div>chessinfo---{{ id }}</div>
+  </div>
 </template>
 
 <script>
+import detailHeader from '@/components/DetailHeader.vue'
+
 export default {
   name: 'chessInfo',
-  props: ['id']
+  props: ['id'],
+  components: {
+    detailHeader
+  }
 }
 </script>
 
